28b46bca88241b0b99a780540e3df90768fb4aa5
htet htet san <htethtetsan57@gmail.com>
2021-01-14 13:53:47 +0630

    reply issue

--- src/app/feedback/feedback.component.ts
+++ src/app/feedback/feedback.component.ts
@@ -16,7 +16,7 @@ export class FeedbackComponent implements OnInit {
 
   replied: string;
   feedbackBoId = "";
-  
+
   replyMessage = "";
   feedbacks = [];
 
@@ -37,165 +37,165 @@ export class FeedbackComponent implements OnInit {
     this.getAllFeedbacks();
   }
 
-  getAllFeedbacks(){
-  
-      const header: HttpHeaders = new HttpHeaders({
-        token: this.ics.token
-      });
-  
-      const url: string = this.ics.apiRoute + "/operation/getFeedbacks";
-      this.http.request("get", url,
-        {
-          headers: header
-        }
-      ).subscribe(
-        (data: any) => {
-          console.log(data)
-          if(data.message == "Unauthorized Request")
-            this.loginDialog();
-          this.feedbacks = data.feedbacks;
-  
-        },
-        error => {
-          console.warn("error: ", error);
-        });
-    }
+  getAllFeedbacks() {
 
-    loginDialog() {
-      const dialogRef = this.dialog.open(LoginDialog, {
-        data:{ 
-          "title": "Please login first!!",
-        }
-      });
-  
-      dialogRef.afterClosed().subscribe(result => {
-        console.log('The dialog was closed');
-      });
-  
-    }
+    const header: HttpHeaders = new HttpHeaders({
+      token: this.ics.token
+    });
 
-    save(){
-      this.submitted = true;
-      this.replyMessage = this.replyForm.value;
-      if (this.replyForm.invalid) {
-        return;
+    const url: string = this.ics.apiRoute + "/operation/getFeedbacks";
+    this.http.request("get", url,
+      {
+        headers: header
       }
-      console.log(this.replyMessage)
-      const json = {"feedbackId":this.feedbackBoId,"message":this.replyMessage}
-      const url = this.ics.apiRoute + "/operation/reply";
-      const header: HttpHeaders = new HttpHeaders({
-        token: this.ics.token
+    ).subscribe(
+      (data: any) => {
+        console.log(data)
+        if (data.message == "Unauthorized Request")
+          this.loginDialog();
+        this.feedbacks = data.feedbacks;
+
+      },
+      error => {
+        console.warn("error: ", error);
       });
-      this.http.post(url, json,
-        {
-          headers: header
+  }
+
+  loginDialog() {
+    const dialogRef = this.dialog.open(LoginDialog, {
+      data: {
+        "title": "Please login first!!",
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+
+  }
+
+  save() {
+    this.submitted = true;
+    if (this.replyForm.invalid) {
+      return;
+    }
+
+    const json = { "feedbackId": this.feedbackBoId, "message": this.replyForm.value.message}
+    console.log("json !!!!!!!!!!", json)
+    const url = this.ics.apiRoute + "/operation/reply";
+    const header: HttpHeaders = new HttpHeaders({
+      token: this.ics.token
+    });
+    this.http.post(url, json,
+      {
+        headers: header
+      }
+    ).subscribe(
+      (data: any) => {
+        console.log(data)
+        if (data.message == "Unauthorized Request")
+          this.loginDialog();
+        if (data.status == "1") {
+          this.successDialog();
+          this.replyInput.nativeElement.value = ' ';
+
         }
-      ).subscribe(
-        (data: any) => {
-          console.log(data)
-          if(data.message == "Unauthorized Request")
-            this.loginDialog();
-            if (data.status == "1"){
-              this.successDialog();
-              this.replyInput.nativeElement.value = ' ';
-              
-            }
-            else this.failDialog(data.msg);
-  
-        },
-        error => {
-          console.warn("error: ", error);
-        });
-    }
-  
-    cancel(event){
-      this.replied = "false";
-      event.target.style.color = '#0000FF';
-    }
+        else this.failDialog(data.msg);
 
-    reply(event){
-      this.replied = "true";
-      // this.feedback.boId = Reply.textContent;
-      // console.log(Reply.textContent)
-      this.feedbackBoId = event.target.value;
-      event.target.style.color = ' #00cdac';
-      console.log(this.feedbackBoId)
-    
-    }
-
-    failDialog(message) {
-      const dialogRef = this.dialog.open(FailDialog, {
-        data:{ 
-          "title": "Unable to reply!!",
-          "message": message
-        }
+      },
+      error => {
+        console.warn("error: ", error);
       });
-  
-      dialogRef.afterClosed().subscribe(result => {
-        console.log('The dialog was closed');
-      });
-  
-    }
-
-    successDialog() {
-      const dialogRef = this.dialog.open(SuccessDialog, {
-      });
-  
-      dialogRef.afterClosed().subscribe(result => {
-        console.log('The dialog was closed');
-      });
-  
-    }
   }
 
-  @Component({
-    selector: 'login-dialog',
-    templateUrl: './login-dialog.html',
-  })
-  export class LoginDialog {
-  
-    constructor(
-      public dialogRef: MatDialogRef<LoginDialog>,
-      public router: Router,
-      @Inject(MAT_DIALOG_DATA) public data: {title: string}
-    ) { }
-  
-    route(): void {
-      this.dialogRef.close();
-      this.router.navigate(['login']);
-    }
+  cancel(event) {
+    this.replied = "false";
+    event.target.style.color = '#0000FF';
   }
 
-  @Component({
-    selector: 'success-dialog',
-    templateUrl: './success-dialog.html',
-  })
-  export class SuccessDialog {
-  
-    constructor(
-      public dialogRef: MatDialogRef<SuccessDialog>,
-    ) { }
-  
-    onNoClick(): void {
-      this.dialogRef.close();
-    }
-  
+  reply(event) {
+    this.replied = "true";
+    // this.feedback.boId = Reply.textContent;
+    // console.log(Reply.textContent)
+    this.feedbackBoId = event.target.value;
+    event.target.style.color = ' #00cdac';
+    console.log(this.feedbackBoId)
+
   }
-  
-  @Component({
-    selector: 'fail-dialog',
-    templateUrl: './fail-dialog.html',
-  })
-  export class FailDialog {
-  
-    constructor(
-      public dialogRef: MatDialogRef<FailDialog>,
-      @Inject(MAT_DIALOG_DATA) public data: {title: string; message: string}
-    ) { }
-  
-    onNoClick(): void {
-      this.dialogRef.close();
-    }
+
+  failDialog(message) {
+    const dialogRef = this.dialog.open(FailDialog, {
+      data: {
+        "title": "Unable to reply!!",
+        "message": message
+      }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+
   }
+
+  successDialog() {
+    const dialogRef = this.dialog.open(SuccessDialog, {
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+    });
+
+  }
+}
+
+@Component({
+  selector: 'login-dialog',
+  templateUrl: './login-dialog.html',
+})
+export class LoginDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<LoginDialog>,
+    public router: Router,
+    @Inject(MAT_DIALOG_DATA) public data: { title: string }
+  ) { }
+
+  route(): void {
+    this.dialogRef.close();
+    this.router.navigate(['login']);
+  }
+}
+
+@Component({
+  selector: 'success-dialog',
+  templateUrl: './success-dialog.html',
+})
+export class SuccessDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<SuccessDialog>,
+  ) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
+
+@Component({
+  selector: 'fail-dialog',
+  templateUrl: './fail-dialog.html',
+})
+export class FailDialog {
+
+  constructor(
+    public dialogRef: MatDialogRef<FailDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: { title: string; message: string }
+  ) { }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
 
 
